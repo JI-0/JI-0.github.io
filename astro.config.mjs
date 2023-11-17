fae655c0e38734c1b8819855907eedc5cccc0776
JI-0 <janivkovic11111@gmail.com>
2023-11-18 00:07:09 +0900

    Fixed link for GitHub

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ji-0.github.com',
+    site: 'https://ji-0.github.io',
 });
