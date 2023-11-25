b06e90f7f76164651ac9150f44264e8cb92e77b6
JI-0 <janivkovic11111@gmail.com>
2023-11-25 14:44:15 +0900

    Moved script for webrtc to separate file, added button disabled css

---
+++ src/webrtc_demo.ts
@@ -0,0 +1,239 @@
+import { Chart } from './chart.js';
+
+const uri = "wss://the.testingwebrtc.com:3000";
+let username = "";
+let user_info = "";
+let streamerWebsocket : WebSocket;
+let subscriberWebsocket : WebSocket;
+let analyzerWebsocket : WebSocket;
+
+
+const streamerPeerConnections = {};
+let subscriberPeerConnection : RTCPeerConnection;
+const RTCConfig = {
+    iceServers: [
+        {
+            // "urls": "turn:test.testingwebrtc.com:443",
+            // 'username': 'test',
+            // 'credential': 'key1234567890',
+            "urls": "stun:stun.l.google.com:19302",
+        }
+    ]
+};
+//Stream constraints
+let constraints = {
+    video: { facingMode: "user", width: {exact: 640}, height: {exact: 480} },
+    mimeType: 'video/VP8'
+    // audio: true
+};
+
+
+document.getElementById("startStreamerBtn").onclick = startStreamer;
+document.getElementById("connectSubscriberBtn").onclick = connectSubscriber;
+const streamerVideo = document.getElementById("streamer") as HTMLVideoElement;
+const subscriberVideo = document.getElementById("subscriber") as HTMLVideoElement;
+
+
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const charactersLength = characters.length;
+
+function makeID(length = 16) {
+    let result = '';
+    let counter = 0;
+    while (counter < length) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        counter += 1;
+    }
+    return result;
+}
+
+
+//Streamer
+streamerWebsocket.onopen = () => {
+    streamerWebsocket.send("U\n" + username + "\n" + user_info);
+};
+
+streamerWebsocket.onclose = () => {
+};
+
+streamerWebsocket.onmessage = e => {
+    let parts = e.data.split("\n", 5);
+    if (parts[0] == "R") {
+        newSubscriber(parts[1]);
+    } else if (parts[0] == "A") {
+        processAnswer(parts[1], JSON.parse(parts[2]));
+    } else if (parts[0] == "C") {
+        processCandidateStreamer(parts[1], JSON.parse(parts[2]));
+    };
+};
+
+function newSubscriber(id) {
+    const peerConnection = new RTCPeerConnection(RTCConfig);
+    streamerPeerConnections[id] = peerConnection;
+
+    //Set streaming
+    let stream = streamerVideo.srcObject as MediaStream;
+    stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
+
+    //Candidate
+    peerConnection.onicecandidate = e => {
+        if (e.candidate) {
+            streamerWebsocket.send("C\n" + id + "\n" + JSON.stringify(e.candidate));
+        };
+    };
+
+    //Create and send offer
+    peerConnection
+    .createOffer()
+    .then(sdp => peerConnection.setLocalDescription(sdp))
+    .then(() => {
+        streamerWebsocket.send("O\n" + id + "\n" + JSON.stringify(peerConnection.localDescription));
+    });
+
+    peerConnection.onconnectionstatechange = () => {
+        console.log("Disconnecting peer");
+        if (peerConnection.iceConnectionState == 'disconnected') {
+            streamerPeerConnections[id].close();
+            delete streamerPeerConnections[id];
+        };
+    };
+};
+
+function processAnswer(id, answer) {
+    if (id in streamerPeerConnections) {
+        streamerPeerConnections[id].setRemoteDescription(answer);
+    };
+};
+
+function processCandidateStreamer(id, candidate) {
+    streamerPeerConnections[id].addIceCandidate(new RTCIceCandidate(candidate));
+};
+
+
+//Subscriber
+subscriberWebsocket.onopen = () => {
+    subscriberWebsocket.send("R\n" + username);
+};
+
+subscriberWebsocket.onclose = () => {
+};
+
+subscriberWebsocket.onmessage = e => {
+    let parts = e.data.split("\n", 5);
+    if (parts[0] == "O") {
+        processOffer(parts[1], JSON.parse(parts[2]));
+    } else if (parts[0] == "C") {
+        processCandidateSubscriber(parts[1], JSON.parse(parts[2]));
+    };
+};
+
+function processOffer(id, offer) {
+    subscriberPeerConnection = new RTCPeerConnection(RTCConfig);
+    subscriberPeerConnection
+    .setRemoteDescription(offer)
+    .then(() => subscriberPeerConnection.createAnswer())
+    .then(sdp => subscriberPeerConnection.setLocalDescription(sdp))
+    .then(() => {
+        subscriberWebsocket.send("A\n" + id + "\n" + JSON.stringify(subscriberPeerConnection.localDescription));
+    });
+
+    //Stream
+    subscriberPeerConnection.ontrack = e => {
+        subscriberVideo.srcObject = e.streams[0];
+    };
+
+    //Candidate
+    subscriberPeerConnection.onicecandidate = e => {
+        if (e.candidate) {
+            subscriberWebsocket.send("C\n" + id + "\n" + JSON.stringify(e.candidate));
+        };
+    };
+}
+
+
+function processCandidateSubscriber(_, candidate) {
+    subscriberPeerConnection
+    .addIceCandidate(new RTCIceCandidate(candidate))
+    .catch(e => console.error(e));
+};
+
+
+//UI
+function startStreamer() {
+    (document.getElementById("resolution") as HTMLSelectElement).disabled = true;
+    (document.getElementById("startStreamerBtn") as HTMLButtonElement).disabled = true;
+    (document.getElementById("connectSubscriberBtn") as HTMLButtonElement).disabled = false;
+    if (screen.width > 600) {
+        if ((document.getElementById("resolution") as HTMLSelectElement).value == "1920x1080") {
+            constraints.video.width = {exact: 1920}
+            constraints.video.height = {exact: 1080}
+        } else if ((document.getElementById("resolution") as HTMLSelectElement).value == "1280x720") {
+            constraints.video.width = {exact: 1280}
+            constraints.video.height = {exact: 720}
+        };
+    } else {
+        if ((document.getElementById("resolution") as HTMLSelectElement).value == "1920x1080") {
+            constraints.video.width = {exact: 1080}
+            constraints.video.height = {exact: 1920}
+        } else if ((document.getElementById("resolution") as HTMLSelectElement).value == "1280x720") {
+            constraints.video.width = {exact: 720}
+            constraints.video.height = {exact: 1280}
+        } else {
+            constraints.video.width = {exact: 480}
+            constraints.video.height = {exact: 640}
+        };
+    };
+    //Set username
+    username = makeID()
+    //Start websocket
+    streamerWebsocket = new WebSocket(uri);
+    //Setup streaming
+    navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(stream => {
+        streamerVideo.srcObject = stream;
+    })
+    .catch(error => console.error(error));
+};
+
+function connectSubscriber() {
+    (document.getElementById("connectSubscriberBtn") as HTMLButtonElement).disabled = true;
+    subscriberWebsocket = new WebSocket(uri);
+};
+
+
+//Chart
+let packetChart = (document.getElementById("packetChart") as HTMLCanvasElement).getContext("2d");
+
+const xValues = [...Array(121).keys()];
+let chart = new Chart(packetChart, {
+    type: "line",
+    data: {
+        labels: xValues,
+        datasets: [{
+            label: "Maximum",
+            data: [],
+            borderColor: "green",
+            fill: false
+        },{
+            label: "Average",
+            data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
+            borderColor: "blue",
+            fill: false
+        },{
+            label: "Minimum",
+            data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
+            borderColor: "red",
+            fill: false
+        }]
+    },
+});
+
+
+//Unload
+window.onunload = window.onbeforeunload = () => {
+    subscriberPeerConnection.close();
+    streamerWebsocket.close();
+    subscriberWebsocket.close();
+    analyzerWebsocket.close();
+};
